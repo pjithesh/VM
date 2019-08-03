0d84179b915b2ca8466402f2423fd876563030f7
Jithesh <pjithesh87@gmail.com>
2019-08-03 15:36:33 -0500

    Final version-Ready to Merge

--- a.py
+++ a.py
@@ -1,5 +1,6 @@
 Print("Hi Jithu")
 Print("A is my file")
 Print ("A is in Branch B Now")
+Print("Code A is finalised and ready to be merged")
 
 
--- b.py
+++ b.py
@@ -1,2 +1,4 @@
 Print("Branch A")
 Print("Code B is also going to Branch B")
+Print("Code B is finalised and ready to merge")
+
